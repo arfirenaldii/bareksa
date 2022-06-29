1023a5f9041ea2de56f2ae3a5eb87e24ffeb071d
arfi.justika <arfi@justika.com>
2022-06-29 10:04:41 +0700

    remove unused code

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,18 +12,13 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from 'containers/Home';
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
 
 const AppWrapper = styled.div`
   // max-width: calc(768px + 16px * 2);
   margin: 0 auto;
   display: flex;
   min-height: 100%;
-  padding: 0 16px;
   flex-direction: column;
 `;
 
@@ -36,14 +31,10 @@ export default function App() {
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      {/* <Header /> */}
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/home" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      {/* <Footer /> */}
     </AppWrapper>
   );
 }
